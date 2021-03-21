a127be6e54368d26d6961af3069d8c7e43bf11db
minami <minaminaoy@gmail.com>
2021-03-22 03:21:58 +0900

    AC

--- rust/atcoder/abc196/src/bin/a.rs
+++ rust/atcoder/abc196/src/bin/a.rs
@@ -1,1 +1,19 @@
+#[allow(unused_imports)]
+use proconio::input;
+#[allow(unused_imports)]
+use proconio::marker::{Bytes, Chars, Isize1, Usize1};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 
+#[allow(non_snake_case)]
+fn main() {
+    input! {
+        a:isize,
+        b:isize,
+        c:isize,
+        d:isize
+    }
+    println!("{}", b-c);
+}
